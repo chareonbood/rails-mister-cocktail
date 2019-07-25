83abd2e122ec264d30936dd3d675efb8494d2c7a
steven chareonbood <stoodsnob@gmail.com>
2019-07-25 17:18:07 +0800

    d

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
+
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
